388c8bec77cefcee8bf6a8b8b1a6211d4f76ecd6
PoeHaH <hello@maartenbelmans.com>
2014-03-25 19:54:55 +0100

    Update devicedetector-production.js

--- devicedetector-production.js
+++ devicedetector-production.js
@@ -2,7 +2,7 @@ var deviceDetector = (function (ua)
 {
     var detect = (function()
     {
-		if(/(ipad|tablet|android|(windows(.)*touch)|kindle|playbook)/.test(ua))
+		if(/(ipad|tablet|(android(?!.*mobile))|(windows(.)*touch)|kindle|playbook)/.test(ua))
             return 'tablet';
         else
 			if(/(mobi|ipod|phone|blackberry|opera mini|fennec|minimo|symbian|psp|skyfire|s40Ovibrowser|blazer|bolt|gobrowser|iris|maemo|semc|teashark|uzard )/.test(ua))            
