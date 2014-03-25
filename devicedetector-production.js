b81708b5ac749a095addb9ebe94d7ebcdddd923b
PoeHaH <hello@maartenbelmans.com>
2014-03-25 21:01:35 +0100

    Update devicedetector-production.js
    added kindle fire's silk browser for tablet
    added check for puffin browser
    updated windows tablet detection.
    added archos,nintendo ds to mobile list.

--- devicedetector-production.js
+++ devicedetector-production.js
@@ -2,10 +2,10 @@ var deviceDetector = (function (ua)
 {
     var detect = (function()
     {
-		if(/(ipad|tablet|(android(?!.*mobile))|(windows(.)*touch)|kindle|playbook)/.test(ua))
+		if(/(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(ua))
             return 'tablet';
         else
-			if(/(mobi|ipod|phone|blackberry|opera mini|fennec|minimo|symbian|psp|skyfire|s40Ovibrowser|blazer|bolt|gobrowser|iris|maemo|semc|teashark|uzard )/.test(ua))            
+			if(/(mobi|ipod|phone|blackberry|opera mini|fennec|minimo|symbian|psp|nintendo ds|archos|skyfire|puffin|s40Ovibrowser|blazer|bolt|gobrowser|iris|maemo|semc|teashark|uzard)/.test(ua))            
                 return 'phone';
             else return 'desktop';
     });
