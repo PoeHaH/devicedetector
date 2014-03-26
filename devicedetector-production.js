630dfa7285bef18786671c465b26fec611df4cbc
PoeHaH <hello@maartenbelmans.com>
2014-03-26 13:11:50 +0100

    Update devicedetector-production.js

--- devicedetector-production.js
+++ devicedetector-production.js
@@ -8,7 +8,7 @@ var deviceDetector = (function ()
 		if(/(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(ua))
             		return 'tablet';
         	else
-			if(/(mobi|ipod|phone|blackberry|opera mini|fennec|minimo|symbian|psp|nintendo ds|archos|skyfire|puffin|s40Ovibrowser|blazer|bolt|gobrowser|iris|maemo|semc|teashark|uzard )/.test(ua))            
+			if(/(mobi|ipod|phone|blackberry|opera mini|fennec|minimo|symbian|psp|nintendo ds|archos|skyfire|puffin|blazer|bolt|gobrowser|iris|maemo|semc|teashark|uzard)/.test(ua))            
                 		return 'phone';
             		else return 'desktop';
     });
