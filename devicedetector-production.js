bdc581385a005ac2c3ccbe7649ec6b8083e0dc3a
PoeHaH <hello@maartenbelmans.com>
2014-03-26 13:09:26 +0100

    Update devicedetector-production.js

--- devicedetector-production.js
+++ devicedetector-production.js
@@ -1,13 +1,16 @@
-var deviceDetector = (function (ua)
+var deviceDetector = (function ()
 {
-    var detect = (function()
-    {
+	var ua = navigator.userAgent.toLowerCase();
+	var detect = (function(s)
+	{
+		if(s===undefined)s=ua;
+		else ua = s.toLowerCase();
 		if(/(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(ua))
-            return 'tablet';
-        else
-			if(/(mobi|ipod|phone|blackberry|opera mini|fennec|minimo|symbian|psp|nintendo ds|archos|skyfire|puffin|s40Ovibrowser|blazer|bolt|gobrowser|iris|maemo|semc|teashark|uzard)/.test(ua))            
-                return 'phone';
-            else return 'desktop';
+            		return 'tablet';
+        	else
+			if(/(mobi|ipod|phone|blackberry|opera mini|fennec|minimo|symbian|psp|nintendo ds|archos|skyfire|puffin|s40Ovibrowser|blazer|bolt|gobrowser|iris|maemo|semc|teashark|uzard )/.test(ua))            
+                		return 'phone';
+            		else return 'desktop';
     });
     return{
         device:detect(),
@@ -15,4 +18,4 @@ var deviceDetector = (function (ua)
         isMobile:((detect()!='desktop')?true:false),
         userAgent:ua
     };
-}(navigator.userAgent.toLowerCase()));
+}());
